a7179f162b26dfe6f975fb9ab3f3e432df13b32f
Jack <git@jackbuehner.com>
2022-11-20 15:20:43 -0500

    add linkedin and map to poster page

--- src/routes/posters/trees_sedaag_2022/+page.svelte
+++ src/routes/posters/trees_sedaag_2022/+page.svelte
@@ -7,6 +7,10 @@
   29613
 </p>
 
+<p>
+  <a href="https://www.linkedin.com/in/jack-buehner/">LinkedIn (Jack Buehner)</a>
+</p>
+
 <h2>Links</h2>
 
 <p>
@@ -21,4 +25,20 @@
   </a>
   <br />
   <a href="https://github.com/jackbuehner/treetools">TreeTools GitHub</a>
+  <br />
+  <a
+    href="https://furman.maps.arcgis.com/apps/instant/basic/index.html?appid=570201e58892483cb26c4749f1687048"
+    >Detected trees map</a
+  >
 </p>
+
+<h2>Detected trees map</h2>
+<iframe
+  title="Detected trees map"
+  src="https://furman.maps.arcgis.com/apps/instant/basic/index.html?appid=570201e58892483cb26c4749f1687048"
+  width="1000"
+  height="600"
+  frameborder="0"
+  style="border: 0; max-width: calc(100vw - 24px); max-height: calc(100vh - 24px);"
+  allowfullscreen>iFrames are not supported on this page.</iframe
+>
